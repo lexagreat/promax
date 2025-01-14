ca68a9f97c5c388b8091e296e0daf2fa9c418346
NikolayTolkovanov-front <tnrvlg@mail.ru>
2025-01-15 01:46:23 +0300

    правка конструктора

--- components/sections/try.vue
+++ components/sections/try.vue
@@ -1,90 +1,90 @@
 <template>
   <section class="try">
-    <a href="https://alyona-ai-kryukova.github.io/constructor">
-      <div class="container">
-        <div class="try__inner">
-          <h1 class="try__title">ПРИМЕРЬ <span>СВОЙ БУДУЩИЙ</span> ПОЛ</h1>
-          <div class="try-main">
-            <div class="try-btns">
-              <div class="try-btns__item">
-                <div class="i-photo-orange"></div>
-                <div class="try-btns__item-lbl">
-                  <span class="try-btns__item-title">Загрузите свою комнату</span>
-                  <span class="try-btns__item-desc"></span>
-                </div>
-              </div>
-              <div class="try-btns__item">
-                <div class="i-check-black"></div>
-                <div class="try-btns__item-lbl">
-                  <span class="try-btns__item-title">Выберите пол</span>
-                  <span class="try-btns__item-desc">Country Oak</span>
-                </div>
-              </div>
-              <div class="try-btns__item">
-                <div class="i-choice-black"></div>
-                <div class="try-btns__item-lbl">
-                  <span class="try-btns__item-title">Выберите комнату</span>
-                  <span class="try-btns__item-desc">Моя комната</span>
-                </div>
+    <div class="container">
+      <div class="try__inner">
+        <h1 class="try__title">ПРИМЕРЬ <span>СВОЙ БУДУЩИЙ</span> ПОЛ</h1>
+        <div class="try-main">
+          <a href="https://alyona-ai-kryukova.github.io/constructor" class="try-btns">
+            <div class="try-btns__item">
+              <div class="i-photo-orange"></div>
+              <div class="try-btns__item-lbl">
+                <span class="try-btns__item-title">Загрузите свою комнату</span>
+                <span class="try-btns__item-desc"></span>
               </div>
             </div>
-            <div class="try-slider-wrapper">
-              <div class="try-slider">
-                <NuxtLink
-                  to="#"
-                  class="try-slider__item"
-                >
-                  <div class="try-slider__item-img">
-                    <img
-                      src="@/assets/img/try/image_5.webp"
-                      alt="image_5"
-                    />
-                  </div>
-                  <div class="try-slider__item-main">
-                    <h3 class="try-slider__item-subtitle">Moduleo Roots Country Oak 549941</h3>
-                    <h2 class="try-slider__item-title">Country Oak</h2>
-                    <div class="try-slider__item-size">
-                      <span class="i-size-gray"></span>
-                      <span>632 х 158 мм</span>
-                    </div>
-                  </div>
-                </NuxtLink>
-                <NuxtLink
-                  to="#"
-                  class="try-slider__item"
-                >
-                  <div class="try-slider__item-img">
-                    <img
-                      src="@/assets/img/try/image_5.webp"
-                      alt="image_5"
-                    />
-                  </div>
-                  <div class="try-slider__item-main">
-                    <h3 class="try-slider__item-subtitle">Moduleo Roots Country Oak 549941</h3>
-                    <h2 class="try-slider__item-title">Country Oak 2</h2>
-                    <div class="try-slider__item-size">
-                      <span class="i-size-gray"></span>
-                      <span>632 х 158 мм</span>
-                    </div>
-                  </div>
-                </NuxtLink>
-              </div>
-              <div class="try-slider-control">
-                <div class="try-slider-control__prev"><span class="i-prev-black"></span></div>
-                <div class="try-slider-control__next"><span class="i-next-black"></span></div>
-              </div>
-              <div class="try-slider-more">
-                <!-- link of the current slide -->
-                <NuxtLink to="#">Подробнее</NuxtLink>
+            <div class="try-btns__item">
+              <div class="i-check-black"></div>
+              <div class="try-btns__item-lbl">
+                <span class="try-btns__item-title">Выберите пол</span>
+                <span class="try-btns__item-desc">Country Oak</span>
               </div>
             </div>
-            <div class="try-img-wrapper">
-              <div class="try-img">
-                <img
-                  src="@/assets/img/try/try-img.webp"
-                  alt="try-img"
-                />
+            <div class="try-btns__item">
+              <div class="i-choice-black"></div>
+              <div class="try-btns__item-lbl">
+                <span class="try-btns__item-title">Выберите комнату</span>
+                <span class="try-btns__item-desc">Моя комната</span>
               </div>
+            </div>
+          </a>
+          <div class="try-slider-wrapper">
+            <div class="try-slider">
+              <NuxtLink
+                to="https://alyona-ai-kryukova.github.io/constructor"
+                class="try-slider__item"
+              >
+                <div class="try-slider__item-img">
+                  <img
+                    src="@/assets/img/try/image_5.webp"
+                    alt="image_5"
+                  />
+                </div>
+                <div class="try-slider__item-main">
+                  <h3 class="try-slider__item-subtitle">Moduleo Roots Country Oak 549941</h3>
+                  <h2 class="try-slider__item-title">Country Oak</h2>
+                  <div class="try-slider__item-size">
+                    <span class="i-size-gray"></span>
+                    <span>632 х 158 мм</span>
+                  </div>
+                </div>
+              </NuxtLink>
+              <NuxtLink
+                to="https://alyona-ai-kryukova.github.io/constructor"
+                class="try-slider__item"
+              >
+                <div class="try-slider__item-img">
+                  <img
+                    src="@/assets/img/try/image_5.webp"
+                    alt="image_5"
+                  />
+                </div>
+                <div class="try-slider__item-main">
+                  <h3 class="try-slider__item-subtitle">Moduleo Roots Country Oak 549941</h3>
+                  <h2 class="try-slider__item-title">Country Oak 2</h2>
+                  <div class="try-slider__item-size">
+                    <span class="i-size-gray"></span>
+                    <span>632 х 158 мм</span>
+                  </div>
+                </div>
+              </NuxtLink>
+            </div>
+            <div class="try-slider-control">
+              <div class="try-slider-control__prev"><span class="i-prev-black"></span></div>
+              <div class="try-slider-control__next"><span class="i-next-black"></span></div>
+            </div>
+            <div class="try-slider-more">
+              <!-- link of the current slide -->
+              <NuxtLink to="#">Подробнее</NuxtLink>
+            </div>
+          </div>
+          <div class="try-img-wrapper">
+            <div class="try-img">
+              <img
+                src="@/assets/img/try/try-img.webp"
+                alt="try-img"
+              />
+            </div>
+            <a href="https://alyona-ai-kryukova.github.io/constructor">
               <div class="try-img-btns">
                 <div class="try-img-btns__item btn_try_plus">
                   <span class="i-plus-white"></span>
@@ -99,12 +99,12 @@
                   <span class="i-img-burger"></span>
                 </div>
               </div>
-              <div class="try-unload _btn">Выгрузить</div>
-            </div>
+            </a>
+            <div class="try-unload _btn">Выгрузить</div>
           </div>
         </div>
       </div>
-    </a>
+    </div>
   </section>
 </template>
 <script setup>
